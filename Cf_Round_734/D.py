4324891ccb0818ef9561aa4b9607fd13565b5aa3
Bambam <bambamshivam@gmail.com>
2021-07-26 18:57:38 +0530

    Q_D

--- Cf_Round_734/D.py
+++ Cf_Round_734/D.py
@@ -1,24 +1,21 @@
 import math;import heapq;import string;from collections import deque;import sys;input=sys.stdin.readline;S=lambda:input();I=lambda:int(S());M=lambda:map(int,S().split());L=lambda:list(M());H=1000000000+7
 for _ in range(I()):
 	n,m,k=M()
-	if n%2==0 and k%2==1:
-		print("NO")
-	elif n%2==0 and m%2==0 and k%2==0:
-		print("YES")
-	elif n%2==0 and m%2==1 and k%2==0:
-		if m==1:
-			print("NO")
-		else:
-			print("YES")
-	elif n%2==1 and m%2==0 and k%2==0:
-		if (k//(m//2))%2==1:
+	if n%2==0 and m%2==0:
+		if k%2==0:
 			print("YES")
 		else:
 			print("NO")
-	elif n%2==1 and m%2==0 and k%2==1:
-		if k%(m//2)==0 and (k//(m//2))%2==1:
+	elif n%2==0 and m%2==1:
+		k=(n*m)//2 - k
+		n,m=m,n
+		if k>=(m//2) and (k-(m//2))%2==0:
 			print("YES")
 		else:
 			print("NO")
 	else:
-		print("NO")
+		if k>=(m//2) and (k-(m//2))%2==0:
+			print("YES")
+		else:
+			print("NO")
+
