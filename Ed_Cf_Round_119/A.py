6df25d8ef824b896a3ce3aa2414bf8679f29af68
Bambam <bambamshivam@gmail.com>
2021-12-19 19:22:05 +0530

    _

--- Ed_Cf_Round_119/A.py
+++ Ed_Cf_Round_119/A.py
@@ -1,6 +1,3 @@
 import math,sys;input=sys.stdin.readline;S=lambda:input().rstrip();I=lambda:int(S());M=lambda:map(int,S().split());L=lambda:list(M());mod1=1000000007;mod2=998244353
 
-for _ in range(I()):
-	s=S()
-	if s.count('N')==1:print("NO");continue
-	print("YES")
+for _ in range(I()):print("NO" if S().count('N')==1 else "YES")
