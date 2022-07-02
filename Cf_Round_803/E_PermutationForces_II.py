c6683630368c141b71b101fe7d48e3335ac669bb
Bambam <bambamshivam@gmail.com>
2022-07-02 21:08:35 +0530

    Cf_Round_803

--- Cf_Round_803/E_PermutationForces_II.py
+++ Cf_Round_803/E_PermutationForces_II.py
@@ -1,11 +1,17 @@
 import math,sys;input=sys.stdin.readline;S=lambda:input().rstrip();I=lambda:int(S());M=lambda:map(int,S().split());L=lambda:list(M());mod1=1000000007;mod2=998244353
+from bisect import bisect_right
 for _ in range(I()):
-    n,s=M();a=L();p=[-1]*n
+    n,s=M();a=L();b=L();f=0;s1=set(b);d=[];ans=1;c=[]
     for i in range(n):
         if b[i]!=-1:
-            if abs(a[i]-b[i])>s:f=1;break
-            s.add(b[i])
-    if f:print(0);continue
+            if a[i]-b[i]>s:f=1;break
+        else:
+            c.append(a[i])
+    c.sort()
     for i in range(1,n+1):
-        if i not in s:l.append(i)
-    
+        if i not in s1:d.append(i)
+    if f:print(0);continue
+    n1=len(d)
+    for i in range(n1):
+        ans=(ans*max(bisect_right(c,d[i]+s)-i,0))%mod2
+    print(ans)
