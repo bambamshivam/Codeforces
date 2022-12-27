ce486def4b9a6ba469960cda6704da93f12e2d96
Bambam <bambamshivam@gmail.com>
2022-12-27 21:53:46 +0530

    Cf_Round_841

---
+++ Cf_Round_841/A_Joey_Takes_Money.py
@@ -0,0 +1,5 @@
+import math,sys;input=sys.stdin.readline;S=lambda:input().rstrip();I=lambda:int(S());M=lambda:map(int,S().split());L=lambda:list(M());mod1=1000000007;mod2=998244353
+for _ in range(I()):
+    n=I();a=L();ans=1
+    for i in a:ans*=i
+    print(2022*(ans+n-1))
